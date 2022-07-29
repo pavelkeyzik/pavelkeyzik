1854331d18c60e1f9e56ac6670380b1903826ccb
Pavel Keyzik <pavel.keyzik@gmail.com>
2022-07-29 09:16:24 +0200

    Add Expense Tracking App to list of projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'Expense Tracking App',
+    description: `My side project to track personal finances with really beautiful inteface that currently in active development`,
+    imgSrc: '/static/images/projects/expense-tracking-app.png',
+    href: 'https://twitter.com/ExpenseApp',
+  },
+  {
     title: 'Slice',
     description: `The best way to find and order pizza from independent shops`,
     imgSrc: '/static/images/projects/slice.png',
