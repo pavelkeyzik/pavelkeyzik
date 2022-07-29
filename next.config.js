6c34c4dbaef98d9cbe3fc2dfb983ec1d791ca73b
Pavel Keyzik <pavel.keyzik@gmail.com>
2022-07-29 11:12:37 +0200

    Add Google Analytics

--- next.config.js
+++ next.config.js
@@ -5,13 +5,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
 `
 
 const securityHeaders = [
